bf307c2b6c60508315c0a458f6403f9eeb89428b
walia6 <awalia2002@gmail.com>
2018-05-22 23:22:22 -0500

    final

--- main.lua
+++ main.lua
@@ -58,7 +58,19 @@ function distance ( x1, y1, x2, y2 )
 end
 
 function love.load()
+	debug=false
+	if not bHeight then
+		love.window.setMode(1920, 1080,{fullscreen=true})
+	end
 	inch=0.0254
+	bHeight=bHeight or 161
+	bWidth=bWidth or 100
+	bCOM=bCOM or 90
+
+	cHeight=bHeight
+	cWidth=bWidth
+	cCOM=bCOM
+
 	currentText=""
 	typed=false
 	lastSlider=nil
@@ -68,10 +80,11 @@ function love.load()
 	winWidth,winHeight=love.graphics.getDimensions()
 	math.randomseed(os.time())
 	variables={
-		r=50,
-		b=128,
-		g=1400000,
-		test=750
+		impactPoint=50,
+		impactForce=1400000,
+		Height=bHeight,
+		Width=bWidth,
+		centerOfMass=bCOM
 	}
 
 	var={}
@@ -85,12 +98,17 @@ function love.load()
 	}
 
 	sliderID={}
-	sliderID.r    =slider.new(100,200,161,050,var.r   ,-50,0075,1)
-	sliderID.g    =slider.new(100,275,161,050,var.g   ,000,2800000,1)
-	sliderID.b    =slider.new(100,350,161,050,var.b   ,000,0255,1)
-	sliderID.test =slider.new(400,100,375,050,var.test,500,1000,1)
+	sliderID.impactPoint    =slider.new(winWidth-200,050,161,030,var.impactPoint,  0,     99,1)
+	sliderID.impactForce    =slider.new(winWidth-200,100,161,030,var.impactForce,  0,5250000,1)
+	sliderID.Width          =slider.new(winWidth-200,150,161,030,var.Width,       30,    300,1)
+	sliderID.Height         =slider.new(winWidth-200,200,161,030,var.Height,      30,    300,1)
+	sliderID.centerOfMass   =slider.new(winWidth-200,250,161,030,var.centerOfMass,10,     90,1)
+
+
 
 	--PHYSICS START
+	beginTime=false
+
 	love.physics.setMeter(50)
 	world=love.physics.newWorld(0,9.81*100,true)
 
@@ -101,18 +119,19 @@ function love.load()
 	objects.ground.body=love.physics.newBody(world,winWidth/2,winHeight-50/2)
 	objects.ground.shape=love.physics.newRectangleShape(winWidth,50)
 	objects.ground.fixture=love.physics.newFixture(objects.ground.body,objects.ground.shape)
-	objects.ground.fixture:setFriction(0.95)
+	objects.ground.fixture:setFriction(5)
 
 
 
 	objects.block={}
-	objects.block.body=love.physics.newBody(world,winWidth/4*3,winHeight-50-74/2*inch*love.physics.getMeter()-25,"dynamic")
-	objects.block.shape=love.physics.newRectangleShape( 1, 1, 81*inch*love.physics.getMeter(),130*inch*love.physics.getMeter(), math.rad(0) )
+	objects.block.body=love.physics.newBody(world,winWidth/4*1,cHeight*inch*love.physics.getMeter(),"dynamic")
+	objects.block.shape=love.physics.newRectangleShape( 1, 1, cWidth*inch*love.physics.getMeter(),cHeight*inch*love.physics.getMeter(), math.rad(0) )
 	objects.block.fixture=love.physics.newFixture(objects.block.body,objects.block.shape,5)
 	blockX,blockY,blockMass,blockInertia=objects.block.body:getMassData( )
-	objects.block.body:setMassData( blockX, blockY, 2656, 4289434 )
+	love.system.setClipboardText((winWidth/4*1)..", "..(cHeight*inch*love.physics.getMeter())..("\n"..blockX..", "..(blockY-(cCOM/100-0.5)*cHeight*inch*love.physics.getMeter())..", "..2560))
+	objects.block.body:setMassData( blockX, blockY-(cCOM/100-0.5)*cHeight*inch*love.physics.getMeter(), 0.08*cWidth*cHeight, 42894000*1.5*(0.5*cHeight+0.5*cWidth)/110 )
 	blockX,blockY,blockMass,blockInertia=objects.block.body:getMassData( )
-	objects.block.fixture:setFriction(0.95)
+	objects.block.fixture:setFriction(5)
 	objects.block.fixture:setRestitution(0)
 
 
@@ -121,6 +140,7 @@ function love.load()
 end
 
 function love.update(dt)
+	bHeight,bWidth,bCOM=variables.Height,variables.Width,variables.centerOfMass
 	if selectedSlider then
 		local mouseX, mouseY = love.mouse.getPosition()
 		if mouseX<sliders[selectedSlider].xPos+10 then
@@ -134,7 +154,7 @@ function love.update(dt)
 	--PHYSICS START
 	world:update(dt*1)
 	if love.keyboard.isDown("space") then
-		objects.block.body:applyForce( variables.g, 0, objects.block.body:getX()-50, objects.block.body:getY()-25+variables.r-1 )
+		objects.block.body:applyForce( variables.impactForce, 0, objects.block.body:getX()-cWidth/2*1.25, objects.block.body:getY()-(variables.impactPoint/100-0.5)*cHeight*inch*love.physics.getMeter() )
 	end
 
 
@@ -147,13 +167,6 @@ function drawSliders()
 		local currentID = k
 		local currentSlider=sliders[currentID]
 		love.graphics.setColor(255,255,255)
-		if currentSlider.varName==var.r then
-			love.graphics.setColor(variables.r,0,0)
-		elseif currentSlider.varName==var.g then
-			love.graphics.setColor(0,variables.g,0)
-		elseif currentSlider.varName==var.b then
-			love.graphics.setColor(0,0,variables.b)
-		end
 		
 		love.graphics.rectangle("fill", sliders[currentID].xPos, sliders[currentID].yPos, sliders[currentID].width, sliders[currentID].height)
 		love.graphics.setColor(255,0,0)
@@ -163,6 +176,8 @@ function drawSliders()
 		sliders[currentID].circY=sliders[currentID].yPos+sliders[currentID].height/2
 		sliders[currentID].pixVal=(sliders[currentID].upperBound-sliders[currentID].lowerBound)/(sliders[currentID].width-20)
 		variables[sliders[currentID].varName]=roundToNearest(variables[sliders[currentID].varName],sliders[currentID].step) --what is wrong
+		love.graphics.setColor(255,255,255)
+		love.graphics.printf(sliders[currentID].varName..": "..(variables[sliders[currentID].varName]-1), sliders[currentID].xPos, sliders[currentID].yPos-15, sliders[currentID].width, "center")
 	end
 	love.graphics.setColor(255,255,255)
 end
@@ -171,25 +186,35 @@ function love.draw()
 	drawSliders()
 	
 	if selectedSlider then
-		love.graphics.print(selectedSlider,400,315)
+		love.graphics.print(selectedSlider,15,10)
+	end
+	if not debug then
+		TEMPPRINT=love.graphics.print
+		function love.graphics.print()
+		end
 	end
 	cnt=1
 	for k,v in pairs(variables) do
-		love.graphics.print(k..": "..(v-1), 400, 315+15*cnt)
+		love.graphics.print(k..": "..(v-1), 15,10+15*cnt)
 		cnt=cnt+1
 	end--if love.keyboard.isDown("space")
 	cenX,cenY=objects.block.body:getWorldCenter()
-	love.graphics.print("lastX: "..lastX,400,315+15*(cnt)) cnt=cnt+1
-	love.graphics.print("lastY: "..lastY,400,315+15*(cnt)) cnt=cnt+1
-	love.graphics.print("winWidth: "..winWidth,400,315+15*(cnt)) cnt=cnt+1
-	love.graphics.print("winHeight: "..winHeight,400,315+15*(cnt)) cnt=cnt+1
-	love.graphics.print("cenX: "..cenX,400,315+15*(cnt)) cnt=cnt+1
-	love.graphics.print("cenY: "..cenY,400,315+15*(cnt)) cnt=cnt+1
+	love.graphics.print("lastX: "..lastX,15,10+15*(cnt)) cnt=cnt+1
+	love.graphics.print("lastY: "..lastY,15,10+15*(cnt)) cnt=cnt+1
+	love.graphics.print("winWidth: "..winWidth,15,10+15*(cnt)) cnt=cnt+1
+	love.graphics.print("winHeight: "..winHeight,15,10+15*(cnt)) cnt=cnt+1
+	love.graphics.print("cenX: "..cenX,15,10+15*(cnt)) cnt=cnt+1
+	love.graphics.print("cenY: "..cenY,15,10+15*(cnt)) cnt=cnt+1
 	if love.keyboard.isDown("space") then
-		love.graphics.print("space",400,315+15*(cnt)) cnt=cnt+1
+		love.graphics.print("space",15,10+15*(cnt)) cnt=cnt+1
 	end
 	if blockInertia then
-		love.graphics.print("blockInertia: "..blockInertia,400,315+15*(cnt)) cnt=cnt+1
+		love.graphics.print("blockInertia: "..blockInertia,15,10+15*(cnt)) cnt=cnt+1
+	end
+	love.graphics.print("time: "..(os.clock()),15,10+15*(cnt)) cnt=cnt+1
+	love.graphics.print(130*inch*love.physics.getMeter(),15,10+15*(cnt)) cnt=cnt+1
+	if not debug then
+		love.graphics.print=TEMPPRINT
 	end
 	love.graphics.setColor(255,255,255)
 
@@ -202,7 +227,11 @@ function love.draw()
 	love.graphics.setColor(255,0,0)
 	love.graphics.rectangle("fill", cenX-2, cenY-2, 4, 4)
 	love.graphics.setColor(0,255,0)
-	love.graphics.rectangle("fill",objects.block.body:getX()-50-2,objects.block.body:getY()-25+variables.r-1,4,4)
+	love.graphics.line(objects.block.body:getX()-cWidth/2*1.25-2-50, objects.block.body:getY()-(variables.impactPoint/100-0.5)*cHeight*inch*love.physics.getMeter()-2, objects.block.body:getX()-cWidth/2*1.25-2, objects.block.body:getY()-(variables.impactPoint/100-0.5)*cHeight*inch*love.physics.getMeter()-2)
+	love.graphics.line(objects.block.body:getX()-cWidth/2*1.25,objects.block.body:getY()-(variables.impactPoint/100-0.5)*cHeight*inch*love.physics.getMeter(),objects.block.body:getX()-cWidth/2*1.25-15,objects.block.body:getY()-(variables.impactPoint/100-0.5)*cHeight*inch*love.physics.getMeter()-15)
+	love.graphics.line(objects.block.body:getX()-cWidth/2*1.25,objects.block.body:getY()-(variables.impactPoint/100-0.5)*cHeight*inch*love.physics.getMeter(),objects.block.body:getX()-cWidth/2*1.25-15,objects.block.body:getY()-(variables.impactPoint/100-0.5)*cHeight*inch*love.physics.getMeter()+15)
+	love.graphics.setColor(0,0,255)
+	love.graphics.rectangle("fill",objects.block.body:getX()-2,objects.block.body:getY()-2,4,4)
 
 	--PHYSICS END
 end
@@ -242,5 +271,9 @@ function love.keypressed(key)
 			currentText=""
 			typed=false
 		end
+	end
+
+	if key=="r" then
+		love.load()
 	end
 end
